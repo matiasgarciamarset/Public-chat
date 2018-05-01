5599ec48b505dab73d933f4dae57b2e5304d53fb
AJS2277 <kawaii1548783232>
2018-05-01 16:51:03 -0300

    solucionado problema al crear una sala privada de chat corregido estilo boton enviar agregada maqueta de lista de contactos

--- ChatWeb/Model/User.cs
+++ ChatWeb/Model/User.cs
@@ -1,6 +1,19 @@
-public class User
+using System;
+
+public class User : IComparable
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public string ConnectionId { get; set; }
+
+    public int CompareTo(object obj)
+    {
+        if (obj == null) return 1;
+
+        User otherUser = obj as User;
+        if (otherUser != null)
+            return Id.CompareTo(otherUser.Id);
+        else
+            throw new ArgumentException("Object is not a User");
+    }
 }
